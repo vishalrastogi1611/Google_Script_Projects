69b14c7363897424a457cd0820a43c29f53a4fe0
Vishal Rastogi <vishalrastogi1611@gmail.com>
2025-04-13 12:55:00 +0530

    Update HighLighter_by_Profit_Project.py

--- HighLighter_by_Profit_Project.py
+++ HighLighter_by_Profit_Project.py
@@ -1,2 +1,26 @@
-Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+function myFunction() {
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+  var LastRow = sheet.getLastRow();
+  sheet.getRange("H1").setValue("Profit");
+
+  var SalesRange = sheet.getRange("F2:F"+LastRow);
+
+  var CostRange = sheet.getRange("G2:G"+LastRow);
+
+  var ProfitRange = sheet.getRange("H2:H"+LastRow);
+
+  var SalesValues = SalesRange.getValues();
+
+  var CostValues = CostRange.getValues();
+
+  for(var i=0; i<LastRow; i++){
+    ProfitRange.getCell(i+1,1).setValue(SalesValues[i][0]-CostValues[i][0]);
+
+    if(SalesValues[i][0]-CostValues[i][0] < 70){
+      var rowtohighlight = i+2;
+      sheet.getRange(rowtohighlight,1,1,8).setBackground("#FF0000")
+    }
+  }
+
+  
+}
